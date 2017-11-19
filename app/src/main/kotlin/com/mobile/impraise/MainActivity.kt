9c2772cf966f2941b9ef71be9561625fda8c5063
Aftab Ahmad <aahmad@tribalscale.com>
2017-11-19 12:36:24 -0500

    Add Kotlin.

---
+++ app/src/main/kotlin/com/mobile/impraise/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mobile.impraise
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by aftab on 2017-11-16.
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
